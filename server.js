56a5a651e1746ae08b2390172a37b281d2c32aee
Eda Tuncer <edatezcanli@gmail.com>
2020-08-10 19:14:16 -0700

    heroku fix

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+mongoose.connect (process.env.MONGODB_URI || "mongodb://user:e123456@ds053126.mlab.com:53126/heroku_rmqn9qf3", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
 
 app.use(logger("dev"));
 
