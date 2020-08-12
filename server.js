9d3ab17b208cfb95f187814fb32bdc610fdd5ba6
Eda Tuncer <edatezcanli@gmail.com>
2020-08-11 21:57:48 -0700

    heroku

--- server.js
+++ server.js
@@ -7,11 +7,10 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-mongoose.connect (process.env.MONGODB_URI || "mongodb://user:e123456@ds053126.mlab.com:53126/heroku_rmqn9qf3", {
+mongoose.connect (process.env.MONGODB_URI || "mongodb://user-1:e123456@ds053126.mlab.com:53126/heroku_rmqn9qf3", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
 
 app.use(logger("dev"));
 
@@ -21,10 +20,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
